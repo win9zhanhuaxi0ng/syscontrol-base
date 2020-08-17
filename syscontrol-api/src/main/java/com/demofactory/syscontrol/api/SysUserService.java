23d731ba36164b084851f46276d72016e6dc1fb7
fuhanxiang <FHX100279a>
2020-08-17 15:39:59 +0800

    测试登录功能

--- syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
@@ -2,8 +2,12 @@ package com.demofactory.syscontrol.api;
 
 import com.demofactory.syscontrol.domain.SysUser;
 
+import java.time.LocalDateTime;
+
 public interface SysUserService {
 
     SysUser loginByAccountAndPassword(String account,String password);
-    int registerSysUser(String account,String password);
+    int registerSysUser(String account,String password,String pwdHint);
+    void updateLastLoginTime(LocalDateTime lastLoginTime, String account);
+    LocalDateTime findLastLoginTimeByAccount(String account);
 }
--- syscontrol-dao/src/main/java/com/demofactory/syscontrol/dao/SysUserDao.java
+++ syscontrol-dao/src/main/java/com/demofactory/syscontrol/dao/SysUserDao.java
@@ -4,9 +4,14 @@ import com.demofactory.syscontrol.domain.SysUser;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface SysUserDao {
 
     SysUser findByAccount(String account);
-    void insertNewSysUser(@Param("account") String account,@Param("password") String password);
+    void insertNewSysUser(@Param("account") String account,@Param("password") String password,@Param("pwd_hint") String pwdHint);
+    void updateLastLoginTime(@Param("last_login_time") LocalDateTime lastLoginTime,@Param("account") String account);
+    LocalDateTime findLastLoginTimeByAccount(@Param("account") String account);
 }
