b673ac65aa4b20964a2cb3e1f8d253af63f8e52e
fuhanxiang <FHX100279a>
2020-08-17 16:50:45 +0800

    测试登录功能

--- syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
@@ -5,9 +5,35 @@ import com.demofactory.syscontrol.domain.SysUser;
 import java.time.LocalDateTime;
 
 public interface SysUserService {
-
+    /**
+     * 登录页面实现
+     * @param account
+     * @param password
+     * @return
+     */
     SysUser loginByAccountAndPassword(String account,String password);
+
+    /**
+     * 注册页面实现
+     * @param account
+     * @param password
+     * @param secondaryPwd
+     * @param pwdHint
+     * @return
+     */
     int registerSysUser(String account,String password,String secondaryPwd,String pwdHint);
+
+    /**
+     * 更新上次登录时间
+     * @param lastLoginTime
+     * @param account
+     */
     void updateLastLoginTime(LocalDateTime lastLoginTime, String account);
+
+    /**
+     * 获取上次登录时间
+     * @param account
+     * @return
+     */
     LocalDateTime findLastLoginTimeByAccount(String account);
 }
