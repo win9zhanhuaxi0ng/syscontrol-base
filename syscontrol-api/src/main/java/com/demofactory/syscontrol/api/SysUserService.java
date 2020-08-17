add27dd922117517b724e20e865588b22439732b
fuhanxiang <FHX100279a>
2020-08-17 16:41:43 +0800

    注册功能补充1

--- syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/SysUserService.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public interface SysUserService {
 
     SysUser loginByAccountAndPassword(String account,String password);
-    int registerSysUser(String account,String password,String pwdHint);
+    int registerSysUser(String account,String password,String secondaryPwd,String pwdHint);
     void updateLastLoginTime(LocalDateTime lastLoginTime, String account);
     LocalDateTime findLastLoginTimeByAccount(String account);
 }
