25d6f4c7781d3a96d29214b0c59f568c9b0d4b2a
WCHCCC <1923366766@qq.com>
2020-08-19 18:15:49 +0800

    "用户逻辑启用禁用删除" 接口

--- syscontrol-api/src/main/java/com/demofactory/syscontrol/api/BookService.java
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/BookService.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 public interface BookService extends IService<Books> {
     /**
-     * 书本插入
+     * 书本插入 book_name domain_id
      * @param books
      * @return
      */
     String insertBook(Books books);
 
     /**
-     * 书本查找
+     * 书本查找 book_name domain_id
      * @param books
      * @return
      */
     List<Books> selectBook(Books books);
 
     /**
-     * 书本删除
+     * 书本删除 book_name domain_id
      * @param books
      * @return
      */
---
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/UserStatusService.java
@@ -0,0 +1,17 @@
+package com.demofactory.syscontrol.api;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.demofactory.syscontrol.domain.SysUser;
+/**
+ * @author:Hanamaru
+ * @description: TODO
+ * @date: 2020/8/19 17:34
+ */
+public interface UserStatusService extends IService<SysUser> {
+    /**
+     * 通过 account 修改Status
+     * @param sysUser
+     * @return
+     */
+    String userStatusUpdate(SysUser sysUser);
+}
