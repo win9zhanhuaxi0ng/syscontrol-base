c90fca33a399eec1244ff625880eee7b58f5cadd
WCHCCC <1923366766@qq.com>
2020-08-19 16:35:39 +0800

    "分配用户" 接口

---
+++ syscontrol-api/src/main/java/com/demofactory/syscontrol/api/AssignUserService.java
@@ -0,0 +1,29 @@
+package com.demofactory.syscontrol.api;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.demofactory.syscontrol.domain.SysUser;
+
+import java.util.List;
+
+/**
+ * @author:Hanamaru
+ * @description: TODO
+ * @date: 2020/8/19 14:11
+ */
+public interface AssignUserService extends IService<SysUser> {
+    /**
+     *通过 domainId orgId 查询 sysUser
+     * @param sysUser
+     * @return List
+     */
+    List<SysUser> selectAssignUser(SysUser sysUser);
+
+    /**
+     * 通过 id account 修改sysUser->domainId&&orgId
+     * @param sysUser
+     * @return
+     */
+    String updateAssignUser(SysUser sysUser);
+
+
+}
--- syscontrol-dao/src/main/java/com/demofactory/syscontrol/dao/AssignUserDao.java
+++ syscontrol-dao/src/main/java/com/demofactory/syscontrol/dao/AssignUserDao.java
@@ -1,11 +1,11 @@
 package com.demofactory.syscontrol.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.demofactory.syscontrol.domain.Books;
+import com.demofactory.syscontrol.domain.SysUser;
 /**
  * @author:Hanamaru
  * @description: TODO
- * @date: 2020/8/19 14:13
+ * @date: 2020/8/19 13:49
  */
-public interface BookDao extends BaseMapper<Books> {
+public interface AssignUserDao extends BaseMapper<SysUser> {
 }
