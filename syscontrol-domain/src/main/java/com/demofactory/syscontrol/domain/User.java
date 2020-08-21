c0f79f7abe61ae3035af8c10e797d832490b1a59
WCHCCC <1923366766@qq.com>
2020-08-21 13:57:02 +0800

    前端传后端测试

---
+++ syscontrol-domain/src/main/java/com/demofactory/syscontrol/domain/User.java
@@ -0,0 +1,15 @@
+package com.demofactory.syscontrol.domain;
+
+import lombok.Data;
+
+/**
+ * @author : Hanamaru
+ * @description: TODO
+ * @date : 2020/8/21 11:40
+ */
+@Data
+public class User {
+    private String name;
+    private String age;
+    private String birth;
+}
